7d2aa728e42bdc99f7e7227a73c01eb69e190a22
Mielecki <mieleckibartek@gmail.com>
2024-10-16 12:08:18 +0200

    add Cursor class

--- src/gui.py
+++ src/gui.py
@@ -7,10 +7,17 @@ class Line:
         self.content = []
         self.length = 0
         self.line = line
+        self.is_first = False
+        self.is_last = False
         self.pack(line)
     
     def pack(self, line):
         self.frame.grid(row = line, sticky="w")
+    
+    def __create_label(self):
+        label = Label(self.frame, font=("UbuntuMono", 24), borderwidth=0, background="white")
+        label.pack(side="left")
+        return label
 
     def __get_character(self, index):
         return self.content[index].cget("text")
@@ -20,7 +27,8 @@ class Line:
             character = " "
         self.content[index].config(text=character)
     
-    def add_character(self, character, label, index):
+    def add_character(self, character, index):
+        label = self.__create_label()
         self.content.append(label)
         self.length += 1
 
@@ -40,9 +48,7 @@ class Line:
 
     def move_labels(self, dest, start_index):
         for i in range(start_index, self.length):
-            label = Label(dest.frame, font=("UbuntuMono", 24), borderwidth=0, background="white")
-            label.pack(side="left")
-            dest.add_character(self.__get_character(i), label, i - start_index)
+            dest.add_character(self.__get_character(i), i - start_index)
         
         for _ in range(start_index, self.length):
             self.content.pop().destroy()
@@ -50,6 +56,73 @@ class Line:
         dest.length = self.length - start_index
         self.length = start_index
 
+
+class Cursor:
+    def __init__(self, line, line_position):
+        self.__line_position = line_position
+        self.__line = line
+        self.__last_cursor = (line, 0) # line, line_position
+        self.__line_changed = False
+    
+    def move_up(self, new_line):
+        if self.__line.is_first:
+            return 0
+        new_line_position = self.__line_position
+        new_position_change = new_line.length
+        if self.__line_position > new_line.length - 1:
+            new_line_position = new_line.length - 1
+            new_position_change = self.__line_position + 1
+        self.__line_changed = True
+        self.__line_position = new_line_position
+        self.__line = new_line
+        self.__highlight()
+        return -new_position_change
+
+    def move_down(self, new_line, new_position=-1):
+        if self.__line.is_last:
+            return 0
+        if new_position != -1:
+            self.__line = new_line
+            self.__line_position = new_position
+            self.__line_changed = True
+            self.__highlight()
+            return 0
+        new_line_position = self.__line_position
+        new_position_change = self.__line.length
+        if self.__line_position > new_line.length - 1:
+            new_line_position = new_line.length
+            new_position_change += new_line.length - self.__line_position
+        self.__line_changed = True
+        self.__line_position = new_line_position
+        self.__line = new_line
+        self.__highlight()
+        return +new_position_change
+    
+    def move_right(self):
+        if self.__line_position == self.__line.length or (self.__line.is_last and self.__line_position == self.__line.length + 1):
+            return 0
+        self.__line_position += 1
+        self.__line_changed = False
+        self.__highlight()
+        return 1
+
+    def move_left(self):
+        if self.__line_position == 0:
+            return 0
+        self.__line_position -= 1
+        self.__line_changed = False
+        self.__highlight()
+        return -1
+    
+    def __highlight(self):
+        if (self.__last_cursor[1] < self.__line.length or self.__line_changed) and self.__last_cursor[1] != -1:
+            self.__last_cursor[0].change_label_color(self.__last_cursor[1], "white")
+        if self.__line_position > 0:
+            self.__line.change_label_color(self.__line_position-1, "gray")
+        self.__last_cursor = (self.__line, self.__line_position-1)
+    
+    def get_line_postion(self):
+        return self.__line_position
 
 class Window:
 
@@ -78,11 +151,10 @@ class Window:
         self.__new_piece = ""
         self.__inserted_recently = False
         self.__current_line = 0
-        self.__line_position = 0
         self.__lines = [Line(self.__content, self.__current_line)]
-        self.__last_cursor = (0, 0)
-        self.__line_changed = False
-
+        self.__get_current_line().is_first = True
+        self.__get_current_line().is_last = True
+        self.__cursor = Cursor(self.__get_current_line(), self.__current_line)
 
     def close(self):
         self.__running = False
@@ -108,18 +180,12 @@ class Window:
         if key in self.__key_history:
             self.__key_history.remove(key)
 
-    def __create_label(self):
-        label = Label(self.__lines[self.__current_line].frame, font=("UbuntuMono", 24), borderwidth=0, background="white")
-        label.pack(side="left")
-        return label
-
     def __update_new_piece(self, character):
         if self.__inserted_recently:
             self.__inserted_recently = False
             self.__insert_new_piece()
-        self.__lines[self.__current_line].add_character(character, self.__create_label(), self.__line_position)
-        self.__line_position += 1
-        self.__highlight_cursor()
+        self.__get_current_line().add_character(character, self.__cursor.get_line_postion())
+        self.__cursor.move_right()
         self.__new_piece += character
         self.__inserted_recently = False
     
@@ -131,80 +197,57 @@ class Window:
             self.__inserted_recently = True
         if character is not None:
             self.__new_piece += character
-            self.__lines[self.__current_line].add_character(character, self.__create_label(), self.__line_position)
-            self.__line_position += 1
-            self.__line_changed = False
+            self.__get_current_line().add_character(character, self.__cursor.get_line_postion())
+            self.__cursor.move_right()
 
         if character == '\n':
             self.__new_line()
-        
-        if character is not None:
-            self.__highlight_cursor()
 
     def __rearrange_lines(self):
         for i, line in enumerate(self.__lines):
             line.pack(i)
         
     def __new_line(self):
-        self.__current_line += 1
-        self.__line_changed = True
-        self.__lines.insert(self.__current_line, Line(self.__content, self.__current_line))
-        self.__rearrange_lines()
-        self.__lines[self.__current_line-1].move_labels(self.__get_current_line(), self.__line_position)
-
-        self.__line_position = 0
+        if self.__get_current_line().is_last:
+            self.__get_current_line().is_last = False
+            self.__current_line += 1
+            self.__lines.insert(self.__current_line, Line(self.__content, self.__current_line))
+            self.__rearrange_lines()
+            self.__lines[self.__current_line-1].move_labels(self.__get_current_line(), self.__cursor.get_line_postion())
+            self.__cursor.move_down(self.__get_current_line())
+            self.__get_current_line().is_last = True
+        else:
+            self.__current_line += 1
+            self.__lines.insert(self.__current_line, Line(self.__content, self.__current_line))
+            self.__rearrange_lines()
+            self.__lines[self.__current_line-1].move_labels(self.__get_current_line(), self.__cursor.get_line_postion())
+            self.__cursor.move_down(self.__get_current_line(), 0)
 
         
     def __move_cursor(self, direction):
         if self.__new_piece:
             self.__insert_new_piece()
         if direction == "left":
-            if self.__line_position > 0:
-                self.__position -= 1
-                self.__line_position -= 1
-                self.__highlight_cursor()
+            self.__position += self.__cursor.move_left()
         elif direction == "right":
-            if self.__line_position < self.__lines[self.__current_line].length-1 or (self.__current_line == len(self.__lines)-1 and self.__line_position < self.__lines[self.__current_line].length):
-                self.__position += 1
-                self.__line_position += 1
-                self.__highlight_cursor()
+            self.__position += self.__cursor.move_right()
         elif direction == "up":
-            if self.__current_line == 0:
-                return
-            new_line_position = self.__line_position
-            new_position_change = self.__lines[self.__current_line - 1].length
-            if self.__line_position > self.__lines[self.__current_line - 1].length-1:
-                new_line_position = self.__lines[self.__current_line - 1].length-1
-                new_position_change = self.__line_position
-            self.__position -= new_position_change
-            self.__line_position = new_line_position
-            self.__current_line -= 1
-            self.__line_changed = True
-            self.__highlight_cursor()
-            self.__line_changed = False
+            change = self.__cursor.move_up(self.__lines[self.__current_line - 1])
+            self.__position += change
+            if change: self.__current_line -= 1
         elif direction == "down":
-            if self.__current_line == len(self.__lines) - 1:
-                return
-            new_line_position = self.__line_position
-            new_position_change = self.__lines[self.__current_line].length
-            if self.__line_position > self.__lines[self.__current_line + 1].length-1:
-                new_line_position = self.__lines[self.__current_line + 1].length-1
-                new_position_change += self.__lines[self.__current_line + 1].length - self.__line_position
-            self.__position += new_position_change
-            self.__line_position = new_line_position
-            self.__current_line += 1
-            self.__line_changed = True
-            self.__highlight_cursor()
-            self.__line_changed = False
+            print(self.__get_current_line().is_last)
+            if self.__get_current_line().is_last: return
+            change = self.__cursor.move_down(self.__lines[self.__current_line + 1])
+            self.__position += change
+            if change: self.__current_line += 1
 
     def __remove_character_backspace(self):
         self.__insert_new_piece()
-        if self.__line_position > 0:
+        if self.__cursor.get_line_postion() > 0:
             self.__piece_table.delete(self.__position-1, 1)
-            self.__lines[self.__current_line].remove_character(self.__line_position-1)
-            self.__line_position -= 1
-            self.__position -= 1
-            self.__highlight_cursor()
+            self.__get_current_line().remove_character(self.__cursor.get_line_postion()-1)
+            self.__position += self.__cursor.move_left()
 
 
     def __create_events(self):
@@ -219,13 +262,6 @@ class Window:
                            "Down": lambda: self.__move_cursor("down"),
                            })
         return key_events
-    
-    def __highlight_cursor(self):
-        if (self.__last_cursor[1] < self.__get_current_line().length or self.__line_changed) and self.__last_cursor[1] != -1:
-            self.__lines[self.__last_cursor[0]].change_label_color(self.__last_cursor[1], "white")
-        if self.__line_position > 0:
-            self.__lines[self.__current_line].change_label_color(self.__line_position-1, "gray")
-        self.__last_cursor = (self.__current_line, self.__line_position-1)
 
     
     def __get_current_line(self):
